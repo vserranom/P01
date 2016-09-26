2d2ee22a0bc6593a928ef54d39954f2a3db4b5b4
victor <victor.resano.21@gmail.com>
2016-09-26 20:34:14 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mi_primera_aplicacion/Hello_World.java
@@ -0,0 +1,13 @@
+package com.example.mi_primera_aplicacion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Hello_World extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello__world);
+    }
+}
